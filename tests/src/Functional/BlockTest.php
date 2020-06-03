f42fdd29a24c7414f2f3dc84c11ff9f044220fe0
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2020-06-03 16:44:31 +0100

    Add test to verify nodeheaderblock fails when content type has no body
    Ref #6
    
    Creates a dummy content type that contains no body field.
    Running this test should generate mink error status code 500.

--- tests/src/Functional/BlockTest.php
+++ tests/src/Functional/BlockTest.php
@@ -33,11 +33,22 @@ class BlockTest extends BrowserTestBase {
   protected function setUp() {
     parent::setUp();
 
+    // Create a dummy content type that we will use for testing.
+    $type = $this->container->get('entity_type.manager')->getStorage('node_type')
+      ->create([
+        'type' => 'dummy',
+        'name' => 'Dummy',
+      ]);
+    $type->save();
+    $this->container->get('router.builder')->rebuild();
+
     $this->adminUser = $this->drupalCreateUser([
       'access content',
       'administer nodes',
       'create localgov_services_page content',
       'edit own localgov_services_page content',
+      'create dummy content',
+      'edit own dummy content',
     ]);
   }
 
@@ -63,6 +74,24 @@ class BlockTest extends BrowserTestBase {
     $this->assertSession()->pageTextContains($title);
     $this->assertSession()->pageTextContains($summary);
     $this->assertSession()->pageTextContains($body);
+  }
+
+  /**
+   * Test blocks display when content type has no body field.
+   */
+  public function testBlocksDisplayWhenNoSummary() {
+    $this->drupalLogin($this->adminUser);
+
+    // Check node title and summary display on a landing page.
+    $title = $this->randomMachineName(8);
+    $edit = [
+      'title[0][value]' => $title,
+      'status[value]' => 1,
+    ];
+    $this->drupalPostForm('/node/add/dummy', $edit, 'Save');
+    $this->drupalGet('/node/1');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains($title);
   }
 
 }
