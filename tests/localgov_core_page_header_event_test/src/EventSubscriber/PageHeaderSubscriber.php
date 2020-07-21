907c77005088d212f3cbb95aee76298de402b99b
Stephen Cox <stephen@agile.coop>
2020-07-21 14:26:35 +0100

    Added tests for page header block. #87

---
+++ tests/localgov_core_page_header_event_test/src/EventSubscriber/PageHeaderSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\localgov_core_page_header_event_test\EventSubscriber;
+
+use Drupal\node\Entity\Node;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\localgov_core\Event\PageHeaderDisplayEvent;
+
+/**
+ * Class PageHeaderSubscriber.
+ *
+ * @package Drupal\localgov_core_page_header_event_test\EventSubscriber
+ */
+class PageHeaderSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      PageHeaderDisplayEvent::EVENT_NAME => ['setPageHeader', 0],
+    ];
+  }
+
+  /**
+   * Set page title and lede.
+   */
+  public function setPageHeader(PageHeaderDisplayEvent $event) {
+
+    // Override title and lede for page1 node content types.
+    if ($event->getEntity() instanceof Node &&
+      $event->getEntity()->bundle() == 'page1'
+    ) {
+      $event->setTitle('Overridden title');
+      $event->setLede('Overridden lede');
+    }
+
+    // Hide page header block for page2 content types.
+    if ($event->getEntity() instanceof Node &&
+      $event->getEntity()->bundle() == 'page2'
+    ) {
+      $event->setVisibility(FALSE);
+    }
+  }
+
+}
