bec6f29eb48a0dd19a9dac6a554dfdd862025260
Adnan <adnan.muhammad@croydon.gov.uk>
2021-03-04 21:05:08 +0000

    Field renaming: Use schema for data copying.
    When renaming fields, we need to copy data between the tables of the old and new
    field.  Drupal's database schema can tell us the tables associated with a field.
    This helps to determine which table's data should be copied to where.

--- src/FieldRenameHelper.php
+++ src/FieldRenameHelper.php
@@ -2,48 +2,46 @@
 
 namespace Drupal\localgov_core;
 
+use Drupal\field\FieldStorageConfigInterface;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\field\Entity\FieldConfig;
-use Drupal\Core\Config\FileStorage;
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Entity\Entity\EntityFormDisplay;
 
+/**
+ * Renames an existing entity field.
+ *
+ * - Creates Field storage for renamed field.
+ * - Updates config dependencies with the new field name.
+ * - Updates entity view display and form display with the new field name.
+ *   Covers even field groups!
+ * - Removes the old field storage.
+ */
 class FieldRenameHelper {
 
   /**
-   * Rename field
-   * @param  string $old_field_name
+   * Rename field.
+   *
+   * @param string $old_field_name
    *   Old field machine name.
-   * @param  string $new_field_name
+   * @param string $new_field_name
    *   New field machine name.
-   * @param  string $entity_type
+   * @param string $entity_type
    *   Entity type name (eg. node, paragraph etc...)
+   *
+   * @see https://www.drupal.org/docs/drupal-apis/update-api/updating-entities-and-fields-in-drupal-8
    */
   public static function renameField(string $old_field_name, string $new_field_name, string $entity_type) {
 
-    // Rename field.
-    // https://www.drupal.org/docs/drupal-apis/update-api/updating-entities-and-fields-in-drupal-8
-
     // Make sure new field name is correctly formatted.
     $new_field_name = substr($new_field_name, 0, 32);
-
-    // Field variables.
-    // @TODO find out how to get the correct table names.
-    $old_table = $entity_type . '__' . $old_field_name;
-    $new_table = $entity_type . '__' . $new_field_name;
-    $revision_old_table = $entity_type . '_revision__' . $old_field_name;
-    $revision_new_table = $entity_type . '_revision__' . $new_field_name;
-
-    // Get config storage.
-    $config_storage = \Drupal::service('config.storage');
-    $config_factory = \Drupal::configFactory();
 
     // Get the old field config.
     $field_storage = FieldStorageConfig::loadByName($entity_type, $old_field_name);
 
     // If the field config does not exist, just return now.
     // (Assume it was deleted intentionally)
-    if (!$field_storage instanceof FieldStorageConfig) {
+    if (!$field_storage instanceof FieldStorageConfigInterface) {
       return;
     }
 
@@ -58,14 +56,13 @@ class FieldRenameHelper {
     $new_field_storage->enforceIsNew(TRUE);
     $new_field_storage->save();
 
-    // Duplicate the field data.
-    $database = \Drupal::database();
-    $prefix = $database->tablePrefix();
-    $sql[] = "INSERT INTO $prefix$new_table SELECT * FROM $prefix$old_table";
-    $sql[] = "INSERT INTO $prefix$revision_new_table SELECT * FROM $prefix$revision_old_table";
-
-    foreach ($sql as $indv_sql) {
-      $database->query($indv_sql);
+    // Copy table data.
+    $copied_tables = self::copyFieldTables($entity_type, $old_field_name, $new_field_name);
+    if (empty($copied_tables)) {
+      \Drupal::service('logger.factory')->get('localgov_core')->warning('Could not copy field data from source %src-field to target %target-field.', [
+        '%src-field' => $old_field_name,
+        '%target-field' => $new_field_name,
+      ]);
     }
 
     // Update the field config on each bundle.
@@ -96,7 +93,7 @@ class FieldRenameHelper {
           $dependent->setComponent($new_field_name, $component);
           $groups = $dependent->getThirdPartySettings('field_group');
           foreach ($groups as $group_key => $group) {
-            foreach($group['children'] as $child_key => $child) {
+            foreach ($group['children'] as $child_key => $child) {
               if ($child == $old_field_name) {
                 $group['children'][$child_key] = $new_field_name;
                 $dependent->setThirdPartySetting('field_group', $group_key, $group);
@@ -117,4 +114,45 @@ class FieldRenameHelper {
 
     return TRUE;
   }
+
+  /**
+   * Copy data from one field's tables to another.
+   */
+  public static function copyFieldTables(string $host_entity_type_id, string $src_field, string $cloned_field): array {
+
+    $copied_tables = [];
+    $db = \Drupal::database();
+    $db_schema = $db->schema();
+    $logger = \Drupal::service('logger.factory')->get('localgov_core');
+
+    // Fetch table schemas for the source field.
+    $key_value_storage = \Drupal::keyValue('entity.storage_schema.sql');
+    $field_storage_key_name = $host_entity_type_id . '.field_schema_data.' . $src_field;
+    $src_table_schema_list = $key_value_storage->get($field_storage_key_name);
+
+    // Copy source field's table data into the cloned field's tables.
+    foreach ($src_table_schema_list as $src_table_name => $src_table_schema) {
+      $cloned_table_name = str_replace($src_field, $cloned_field, $src_table_name);
+      if (!$db_schema->tableExists($cloned_table_name)) {
+        $logger->warning('Cloned Field table does not exists: %table.  Skipping copy operation.', ['%table' => $cloned_table_name]);
+        continue;
+      }
+
+      try {
+        // @todo Can we do better here?
+        $db->query('INSERT INTO {' . $cloned_table_name . '} SELECT * FROM {' . $src_table_name . '}');
+      }
+      catch (\Exception $e) {
+        $logger->warning('Failed to copy table data into cloned field table: %table.  More: %msg', [
+          '%table' => $cloned_table_name,
+          '%msg' => $e->getMessage(),
+        ]);
+      }
+
+      $copied_tables[] = $cloned_table_name;
+    }
+
+    return $copied_tables;
+  }
+
 }
--- tests/src/Kernel/FieldRenameHelperTest.php
+++ tests/src/Kernel/FieldRenameHelperTest.php
@@ -4,13 +4,10 @@ namespace Drupal\Tests\localgov_core\Kernel;
 
 use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
 use Drupal\Tests\node\Traits\NodeCreationTrait;
-use Drupal\node\NodeInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
-use Drupal\Core\DateTime\DrupalDateTime;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\field\Entity\FieldConfig;
-use Drupal\Core\Config\FileStorage;
 use Drupal\localgov_core\FieldRenameHelper;
 use Drupal\KernelTests\KernelTestBase;
 
@@ -55,6 +52,11 @@ class FieldRenameHelperTest extends KernelTestBase {
     ]);
   }
 
+  /**
+   * Tests for FieldRenameHelper::renameField().
+   *
+   * Adds two fields of different types and then renames them.
+   */
   public function testRenameField() {
 
     // Set up node type with the old fields.
@@ -73,30 +75,50 @@ class FieldRenameHelperTest extends KernelTestBase {
       'label'         => 'Test field',
     ])->enforceIsNew(TRUE)
       ->save();
+    FieldStorageConfig::create([
+      'id'          => 'node.field_another_test_field',
+      'field_name'  => 'field_another_test_field',
+      'type'        => 'integer',
+      'entity_type' => 'node',
+    ])->enforceIsNew(TRUE)
+      ->save();
+    FieldConfig::create([
+      'field_name'    => 'field_another_test_field',
+      'entity_type'   => 'node',
+      'bundle'        => 'test_type',
+      'label'         => 'Another test field',
+    ])->enforceIsNew(TRUE)
+      ->save();
 
     // Set up some nodes.
     $test_field_value = $this->randomMachineName(8);
+    $another_test_field_value = 42;
     $test_node = $this->createNode([
       'type'             => 'test_type',
       'title'            => $this->randomMachineName(8),
       'field_test_field' => $test_field_value,
+      'field_another_test_field' => $another_test_field_value,
     ]);
     $test_node_id = $test_node->id();
 
     // Rename the node type fields.
     FieldRenameHelper::renameField('field_test_field', 'renamed_test_field', 'node');
+    FieldRenameHelper::renameField('field_another_test_field', 'another_renamed_test_field', 'node');
 
     // Reload the node for the tests.
     $result_node = Node::load($test_node_id);
 
-    // Asset that the old field name does not exist on the node type.
+    // Asset that the old field names do not exist on the node type.
     $this->assertEmpty($result_node->hasField('field_test_field'));
+    $this->assertEmpty($result_node->hasField('field_another_test_field'));
 
-    // Assert that the new field name does exist on the node type.
+    // Assert that the new field names do exist on the node type.
     $this->assertEquals(TRUE, $result_node->hasField('renamed_test_field'));
+    $this->assertEquals(TRUE, $result_node->hasField('another_renamed_test_field'));
 
     // Assert the field rename is the new name and the data is preserved.
     $this->assertEquals($test_field_value, $result_node->get('renamed_test_field')->value);
+    $this->assertEquals($another_test_field_value, $result_node->get('another_renamed_test_field')->value);
   }
 
 }
