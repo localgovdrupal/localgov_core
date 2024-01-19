134139efdeb1af197c2623a5add56d06eaf3ea9d
Rupert Jabelman <rupert@jabelman.org>
2024-01-19 09:56:12 +0000

    Added test to test failures.

---
+++ tests/src/Functional/FailTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\localgov_core\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * A test that fails.
+ */
+class PageHeaderBlockTest extends BrowserTestBase {
+
+  /**
+   * The test that fails.
+   */
+  public function testFail() {
+    $this->assertEquals(TRUE, FALSE, 'None shall pass.');
+  }
+
+}
